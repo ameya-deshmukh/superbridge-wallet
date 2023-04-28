ce0ef1a7b55ca5e975e6351cb99a0aa4e1ce3c25
Ameya <f20200139@pilani.bits-pilani.ac.in>
2023-04-28 19:23:18 +0530

    erc20 wallet generation and import done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use bip39::{Language, Mnemonic, MnemonicType, Seed};
-use clap::{Args, Parser, Subcommand, ValueEnum};
-use ethers::{
-    prelude::*,
-    signers::{LocalWallet, MnemonicBuilder},
-    types::Address,
+use bitcoin::util::bip32::ExtendedPubKey;
+use bitcoin::{
+    network::constants::Network,
+    util::bip32::{DerivationPath, ExtendedPrivKey},
+    PublicKey,
 };
+use clap::{Args, Parser, Subcommand, ValueEnum};
+use eth_keystore::encrypt_key;
+use hdpath::{Purpose, StandardHDPath};
+use secp256k1::Secp256k1;
+use serde::{Deserialize, Serialize};
+use sha3::{Digest, Keccak256};
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{
     native_token::{lamports_to_sol, sol_to_lamports},
@@ -12,10 +18,11 @@ use solana_sdk::{
     signature::{keypair_from_seed, read_keypair_file, write_keypair_file, Keypair},
     signer::Signer,
 };
+
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 use std::str::FromStr;
-
 const SERVER_URL: &str = "https://api.devnet.solana.com";
 
 #[derive(Debug, Parser)]
@@ -84,30 +91,64 @@ enum Commands {
     },
 }
 
+fn get_extended_keypair(
+    seed: &[u8],
+    hd_path: &StandardHDPath,
+) -> (ExtendedPrivKey, ExtendedPubKey) {
+    let secp = Secp256k1::new();
+    let pk = ExtendedPrivKey::new_master(Network::Bitcoin, seed)
+        // we convert HD Path to bitcoin lib format (DerivationPath)
+        .and_then(|k| k.derive_priv(&secp, &DerivationPath::from(hd_path)))
+        .unwrap();
+    let pubk = ExtendedPubKey::from_private(&secp, &pk);
+    (pk, pubk)
+}
+
 fn generate_keypair_erc() {
-    let mnemonic_type: MnemonicType = MnemonicType::for_word_count(12).unwrap();
-    let mnemonic: Mnemonic = Mnemonic::new(mnemonic_type, Language::English);
+    let mnemonic = Mnemonic::new(MnemonicType::Words12, Language::English);
+    println!("Mnemonic: {}", mnemonic);
 
-    let seed: Seed = Seed::new(&mnemonic, "");
+    // save it as a keystore file
+    let entropy = mnemonic.entropy();
+    // println!("Entropy: {:?}", entropy); //128 bits for 12 words, 256 bits for 24 words
 
-    let wallet = LocalWallet::from_bytes(&seed.as_bytes()).unwrap();
+    let mut rng = rand::thread_rng();
+    let dir = Path::new("./erc");
+    let uuid = encrypt_key(&dir, &mut rng, entropy, "password_to_keystore").unwrap();
+    //println!("File uuid: {}", uuid);
 
-    //to do: extract key-pair from wallet
+    let seed = Seed::new(&mnemonic, ""); //128 hex chars = 512 bits
+    let seed_bytes: &[u8] = seed.as_bytes();
 
-    // Print the mnenomic and the public key
-    println!("Mnemonic: {:?}", mnemonic);
+    let hd_path = StandardHDPath::new(Purpose::Pubkey, 60, 0, 0, 0);
+    let (_pk, pubk) = get_extended_keypair(&seed_bytes, &hd_path);
+
+    println!("Public key: {:?}", pubk.public_key);
+
+    // let _eth_addr = extended_pubk_to_addr(&pubk);
 }
 
 fn import_wallet_erc(phrase: Vec<String>) {
     let phrase: &String = &phrase.join(" ");
 
-    let mnemonic = Mnemonic::from_phrase(phrase, Language::English).unwrap();
+    let mnemonic: Mnemonic = Mnemonic::from_phrase(phrase, Language::English).unwrap();
+    let entropy = mnemonic.entropy();
+    // println!("Entropy: {:?}", entropy); //128 bits for 12 words, 256 bits for 24 words
 
-    let seed = Seed::new(&mnemonic, "");
+    let mut rng = rand::thread_rng();
+    let dir = Path::new("./erc");
+    let uuid = encrypt_key(&dir, &mut rng, entropy, "password_to_keystore").unwrap();
+    //println!("File uuid: {}", uuid);
 
-    let wallet = LocalWallet::from_bytes(&seed.as_bytes()).unwrap();
+    let seed = Seed::new(&mnemonic, ""); //128 hex chars = 512 bits
+    let seed_bytes: &[u8] = seed.as_bytes();
 
-    //to do - extracting key pair from local wallet
+    let hd_path = StandardHDPath::new(Purpose::Pubkey, 60, 0, 0, 0);
+    let (_pk, pubk) = get_extended_keypair(&seed_bytes, &hd_path);
+
+    println!("Public key: {:?}", pubk.public_key);
+
+    // let _eth_addr = extended_pubk_to_addr(&pubk);
 }
 
 fn generate_keypair_spl() {
